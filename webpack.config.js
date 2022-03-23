e66b4e7705dbd1bf518d20811183f56d3ab16859
cqhaisenbao <fanyu9353@gmail.com>
2022-03-23 21:49:57 +0800

    利用webpack5原生功能打包图片文件

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     mode: "development",
     output: {
-        filename: '[name].js',
+        filename: 'js/[name].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
@@ -16,6 +16,18 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             },
+            {
+                test:/\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 8 * 1024,
+                    }
+                },
+                generator: {
+                    filename: 'images/[name].[hash:6].[ext]',
+                }
+            }
         ]
     },
     plugins: [
