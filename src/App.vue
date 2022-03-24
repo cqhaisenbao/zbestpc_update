5b5cc21728a4e2ba0eaebcd098045854c5ee7630
cqhaisenbao <fanyu9353@gmail.com>
2022-03-24 22:15:09 +0800

    vue项目构建前的准备工作

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const message = ref('Hello Vue!')
+</script>
+
+<style></style>
